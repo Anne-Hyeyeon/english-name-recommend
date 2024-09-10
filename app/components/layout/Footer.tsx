fe73ee41c258ce9890a135ceaced5b1d5eb15e00
Hyeyeon(Anne) <khy2106@naver.com>
2024-09-10 23:34:28 +0900

    initial commit

---
+++ app/components/layout/Footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+ return (
+  <footer className="w-full max-w-xl mx-auto py-4 bg-gray-100 text-gray-600 text-center text-sm">
+   <p>© 2024 Anne-Hyeyeon. All rights reserved.</p>
+   <p>dlswptkfkd@gmail.com</p>
+  </footer>
+ );
+};
+
+export default Footer;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,72 +1,35 @@
-import DeployButton from "@/components/deploy-button";
-import { EnvVarWarning } from "@/components/env-var-warning";
-import HeaderAuth from "@/components/header-auth";
-import { ThemeSwitcher } from "@/components/theme-switcher";
-import { hasEnvVars } from "@/utils/supabase/check-env-vars";
-import { GeistSans } from "geist/font/sans";
-import { ThemeProvider } from "next-themes";
-import Link from "next/link";
+import { Metadata } from "next";
 import "./globals.css";
+import Footer from "./components/layout/Footer";
 
-const defaultUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : "http://localhost:3000";
-
-export const metadata = {
-  metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+export const metadata: Metadata = {
+ title: "영어이름 추천 프로그램",
+ description: "춘자가 추천해 주는 영어이름",
+ openGraph: {
+  type: "website",
+  url: "https://localhost:3000",
+  title: "영어이름",
+  description: "추천해 줄게요.",
+ },
+ icons: {
+  icon: "/favicon.ico",
+  shortcut: "/favicon-16x16.png",
+  apple: "/apple-touch-icon.png",
+ },
 };
-
 export default function RootLayout({
-  children,
+ children,
 }: {
-  children: React.ReactNode;
+ children: React.ReactNode;
 }) {
-  return (
-    <html lang="en" className={GeistSans.className} suppressHydrationWarning>
-      <body className="bg-background text-foreground">
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <main className="min-h-screen flex flex-col items-center">
-            <div className="flex-1 w-full flex flex-col gap-20 items-center">
-              <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-                <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
-                  <div className="flex gap-5 items-center font-semibold">
-                    <Link href={"/"}>Next.js Supabase Starter</Link>
-                    <div className="flex items-center gap-2">
-                      <DeployButton />
-                    </div>
-                  </div>
-                  {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
-                </div>
-              </nav>
-              <div className="flex flex-col gap-20 max-w-5xl p-5">
-                {children}
-              </div>
-
-              <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
-                <p>
-                  Powered by{" "}
-                  <a
-                    href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-                    target="_blank"
-                    className="font-bold hover:underline"
-                    rel="noreferrer"
-                  >
-                    Supabase
-                  </a>
-                </p>
-                <ThemeSwitcher />
-              </footer>
-            </div>
-          </main>
-        </ThemeProvider>
-      </body>
-    </html>
-  );
+ return (
+  <html lang="ko" className="h-full">
+   <body className="flex flex-col min-h-screen bg-gray-50 font-sans">
+    <main className="flex-grow flex justify-center items-star t">
+     <div className="w-full max-w-xl bg-white overflow-hidden">{children}</div>
+    </main>
+    <Footer />
+   </body>
+  </html>
+ );
 }
